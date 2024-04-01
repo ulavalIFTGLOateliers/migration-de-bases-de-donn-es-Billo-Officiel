9d67d312ada465be37b6807652cb3a37aad25688
Mohamed Billo Barry <mbbar5@ulaval.ca>
2024-03-31 22:34:36 -0400

    Test

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,36 @@
+CREATE TABLE band(
+    bandName VARCHAR(50),
+    creation YEAR,
+    genre VARCHAR(50),
+    PRIMARY KEY (bandName)
+);
+INSERT INTO band (bandName, creation, genre) VALUES
+('Crazy Duo', 2015, 'rock'),
+('Luna', 2009, 'classical'),
+('Mysterio', 2019, 'pop');
+
+ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician CHANGE singerName musicianName VARCHAR(50);
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
+UPDATE musician SET role = 'vocals', bandName = 'Crazy Duo' WHERE musicianName = 'Alina';
+UPDATE musician SET role = 'guitar', bandName = 'Mysterio' WHERE musicianName = 'Mysterio';
+UPDATE musician SET role = 'percussion', bandName = 'Crazy Duo' WHERE musicianName = 'Rainbow';
+UPDATE musician SET role = 'piano', bandName = 'Luna' WHERE musicianName = 'Luna';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
